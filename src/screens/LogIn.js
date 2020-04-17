c59cca0b0a7b3d1cb95c709ebe344db56c95c9c0
Alina Kazakova <alinakazakova@live.ie>
2020-04-17 17:52:11 +0100

    added tab view to log in

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -3,13 +3,18 @@ import logo from '../logo.svg'
 import '../styles.css'
 import { getAllUsers, setCurrentUserSuccess } from '../actions/user'
 import { connect } from 'react-redux'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
 
 class LogIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: '',
-            password: ''
+            value: {
+                username: '',
+                password: ''
+            },
+            index: 0
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -24,51 +29,66 @@ class LogIn extends React.Component {
     handleSubmit = event => {
         event.preventDefault()
         const { user, setCurrentUserSuccess, history } = this.props
-
-        let userValue = { ...this.state }
-        console.log(user.allUsers)
-        const found = user.allUsers.find(u => u.username === userValue.username)
+        const found = user.allUsers.find(u => u.username === this.state.value.username)
 
         if (found) {
-            if (userValue.password === found.password)
+            if (this.state.value.password === found.password) {
                 setCurrentUserSuccess(found)
-            history().push("/catalog")
+                history().push("/catalog")
+            }
         }
     }
 
     handleChange = event => {
         event.preventDefault();
         if (event.target.name === "username") {
-            this.setState({ username: event.target.value })
+            this.setState({ value: { ...this.state.value, username: event.target.value } })
         } else {
-            this.setState({ password: event.target.value })
+            this.setState({ value: { ...this.state.value, password: event.target.value } })
         }
+    }
+
+    loginComponent = () => {
+        const { history, user } = this.props
+
+        return <div>
+            <img src={logo} className="app-logo" alt="logo" />
+            {this.state.index == 0 && <p className="title">ItemStore</p>}
+            {this.state.index == 1 && <p className="subTitle">ItemStore</p>}
+            {this.state.index == 1 && <p className="subTitle">Admin Login</p>}
+            <form className="formContainer" onChange={this.handleChange} onSubmit={this.handleSubmit} >
+                <label className="subTitle">
+                    Username
+            </label>
+                <input className="input" type="text" name="username" />
+                <label className="subTitle">
+                    Password
+            </label>
+                <input className="input" type="text" name="password" />
+                <input className="bigBtn" type="submit" value="Log In" />
+            </form>
+            {this.state.index == 0 && <form>
+                <input className="bigBtn"
+                    onClick={() => history().push("/register", { current: user.currentUSer })}
+                    type="submit" value="Register" />
+            </form>}
+        </div>
     }
 
 
     render() {
-        const { history } = this.props
-        // console.log(user.allUsers)
         return (
             < div className="container" >
-                <img src={logo} className="app-logo" alt="logo" />
-                <p className="title">ItemStore</p>
-                <form className="formContainer" onChange={this.handleChange} onSubmit={this.handleSubmit} >
-                    <label className="subTitle">
-                        Username
-                            </label>
-                    <input className="input" type="text" name="username" />
-                    <label className="subTitle">
-                        Password
-                            </label>
-                    <input className="input" type="text" name="password" />
-                    <input className="bigBtn" type="submit" value="Log In" />
-                </form>
-                <form>
-                    <input className="bigBtn"
-                        onClick={() => history().push("/register")}
-                        type="submit" value="Register" />
-                </form>
+                <Tabs onSelect={index => this.setState({ index })}>
+                    <TabList> <Tab>Customer</Tab>
+                        <Tab>Administrator</Tab></TabList>
+                    <TabPanel>
+                        <h2>{this.loginComponent()}</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>{this.loginComponent()}</h2>
+                    </TabPanel>
+                </Tabs>
             </div >
         )
     }
