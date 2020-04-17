f470c9acca78b33931b529a01abc23c8ebb42721
Alina Kazakova <alinakazakova@live.ie>
2020-04-17 17:51:57 +0100

    fixed redux store to browser local

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,29 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import userReducer from '../reducers/userReducer'
+import itemReducer from '../reducers/itemReducer'
+import { loadState, saveState } from './localStorage'
+
+const persistedState = loadState()
 
 const rootReducer = combineReducers(
     {
-        user: userReducer
+        user: userReducer,
+        item: itemReducer
     },
-);
+)
 
 const configureStore = () => {
-    return createStore(rootReducer, applyMiddleware(thunk))
+    const store = createStore(rootReducer, persistedState, applyMiddleware(thunk))
+
+    store.subscribe(() => {
+        saveState({
+            user: store.getState().user,
+            item: store.getState().item
+        })
+    })
+
+    return store
 }
 
 export default configureStore
