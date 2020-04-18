526563590655e5ba595a91a7019178d3daf44a69
Alina Kazakova <alinakazakova@live.ie>
2020-04-18 23:35:39 +0100

    created address & payment forms for user

---
+++ src/components/AddressForm.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const AddressForm = ({ value, handleChange }) => {
+
+    return <div>
+        <form className="formContainer" onChange={handleChange} >
+            <p className="subTitle">Delivery Address</p>
+            <div className="formRow"><label className="subTitle">
+                First and Last Name
+            </label>
+                <input value={value.fullName || null} className="input" type="text" name="fullName" /></div>
+            <div className="formRow"><label className="subTitle">
+                Address Line 1
+            </label>
+                <input value={value.address1 || null} className="input" type="text" name="address1" /></div>
+            <div className="formRow"><label className="subTitle">
+                Address Line 2
+            </label>
+                <input value={value.address2 || null} className="input" type="text" name="address2" />
+            </div>
+            <div className="formRow"><label className="subTitle">
+                City
+            </label>
+                <input value={value.city || null} className="input" type="text" name="city" /></div>
+            <div className="formRow"><label className="subTitle">
+                County
+            </label>
+                <input value={value.county || null} className="input" type="text" name="county" /></div>
+            <div className="formRow"><label className="subTitle">
+                Country
+            </label>
+                <input value={value.country || null} className="input" type="text" name="country" /></div>
+            <div style={{ marginTop: 50 }} className="formRow"><label className="subTitle">
+                Save Address
+            </label>
+                <input name="saveAddress" type="checkbox" style={{ height: 30, width: 30 }} onChange={handleChange} checked={value.saveAddress} />
+            </div>
+        </form>
+    </div >
+}
---
+++ src/components/PaymentForm.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+
+export const PaymentForm = ({ value, onSelectChange, handleChange }) => {
+    console.log(value)
+    const months = () => {
+        const list = []
+
+        for (let i = 1; i < 13; i++) {
+            list.push(String(i))
+        }
+
+        return list
+    }
+
+    const years = () => {
+        const list = []
+
+        for (let i = 2020; i < 2030; i++) {
+            list.push(String(i))
+        }
+
+        return list
+    }
+
+    return <div>
+        <form className="formContainer" onChange={handleChange}>
+            <p className="subTitle">Payment Details</p>
+            <div className="formRow"><label className="subTitle">
+                Name on Card
+            </label>
+                <input value={value.fullName || null} className="input" type="text" name="fullName" /></div>
+            <div className="formRow"><label className="subTitle">
+                Card Number
+            </label>
+                <input value={value.cardNumber || null} className="input" type="text" name="cardNumber" /></div>
+            <div className="formRow"> <label className="subTitle">
+                Expiry Date
+            </label>
+                <div><select name="month" className="inputSmall" value={value.month || months()[0]}
+                    onChange={selected => onSelectChange(selected)}>
+                    {months().map(day => {
+                        return <option value={day}>{day}</option>
+                    })}
+                </select> /
+                <select name="year" className="inputSmall" value={value.year || years()[0]}
+                        onChange={selected => onSelectChange(selected)}>
+                        {years().map(month => {
+                            return <option value={month}>{month}</option>
+                        })}
+                    </select></div>
+            </div>
+            <div className="formRow"><label className="subTitle">
+                CCV
+            </label>
+                <input value={value.ccv || null} className="input" type="text" name="ccv" /></div>
+            <div style={{ marginTop: 50 }} className="formRow"><label className="subTitle">
+                Save Payment Details
+            </label>
+                <input name="savePayment" type="checkbox" style={{ height: 30, width: 30 }} onChange={handleChange} checked={value.savePayment} />
+            </div>
+        </form>
+    </div >
+}
