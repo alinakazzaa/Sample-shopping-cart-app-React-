593bc1275ef883d71a44577f2e94118a583d528c
Alina Kazakova <alinakazakova@live.ie>
2020-04-18 23:37:39 +0100

    fixed basketfixed baskets adding and removing redux

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -1,27 +1,26 @@
-import { ADD_ITEM_TO_BASKET, REMOVE_ITEM_FROM_BASKET, UPDATE_ITEM_IN_BASKET } from '../constants'
+import { ADD_ITEM_TO_BASKET, REMOVE_ITEM_FROM_BASKET, UPDATE_ITEM_IN_BASKET, CLEAR_BASKET_STATE } from '../constants'
 
 const initialState = {
-    basketItems: []
+    basketItems: [],
+    basketTotal: 0
 }
 
 const basketReducer = (state = initialState, action) => {
 
-    let items
-
+    let items = [...state.basketItems]
+    let total = state.basketTotal
     switch (action.type) {
 
         case ADD_ITEM_TO_BASKET:
-            items = [...state.basketItems]
             items.splice(items.length, 1, action.item)
-
             return {
                 ...state,
-                basketItems: [...items]
+                basketItems: items,
+                basketTotal: (Number(total) + (Number(action.item.price) * Number(action.item.basketQuantity))).toFixed(2)
             }
 
 
         case UPDATE_ITEM_IN_BASKET:
-            items = [...state.basketItems]
             const index = items.findIndex(item => item.id === action.item.id)
             items.splice(index, 1, action.item)
 
@@ -31,10 +30,17 @@ const basketReducer = (state = initialState, action) => {
             }
 
         case REMOVE_ITEM_FROM_BASKET:
-
+            console.log(total)
             return {
                 ...state,
-                basketItems: [...state.basketItems.filter(item => item.id !== action.item.id)]
+                basketItems: [...state.basketItems.filter(item => item.id !== action.item.id)],
+                basketTotal: (Number(total) - (Number(action.item.price) * Number(action.item.basketQuantity))).toFixed(2)
+            }
+
+        case CLEAR_BASKET_STATE:
+
+            return {
+                ...initialState
             }
         default:
             return state
