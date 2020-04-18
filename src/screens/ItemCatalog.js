f91b282bd1506aa6cfcf73e3e2bfa69f4d1cd93c
Alina Kazakova <alinakazakova@live.ie>
2020-04-18 13:46:23 +0100

    changed layout in main catalog view

--- src/screens/ItemCatalog.js
+++ src/screens/ItemCatalog.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import logo from '../logo.svg'
+import { logOutUser } from '../actions/user'
 import { getAllItems, setCurrentItemSuccess, removeItem } from '../actions/item'
 import { connect } from 'react-redux'
 import { ListItem } from '../components/ListItem'
 import { SortingView } from '../components/SortingView'
+import logoutImg from '../resources/images/log-out.png'
 
 
 class ItemCatalog extends React.Component {
@@ -83,14 +85,20 @@ class ItemCatalog extends React.Component {
         })
     }
 
+    logOut = () => {
+        const { user, logOutUser, history, match } = this.props
+        logOutUser(user.currentUser)
+        history().push("/")
+    }
+
     render() {
         const { sorters, items } = this.state
         const { history, item, user } = this.props
-
+        console.log(this.props)
         return (
             < div className="container" >
-                <img src={logo} className="app-logo" alt="logo" />
-                <div className="itemsContainer">
+                <div className="pageHeader">
+                    <img src={logo} className="app-logo" alt="logo" />
                     <div className="top">
                         <form onChange={this.handleChange} >
                             <div className="lblDiv"><label className="subTitle">Search</label></div>
@@ -99,21 +107,24 @@ class ItemCatalog extends React.Component {
                         {user.currentUser.admin && <button className="addBtn"
                             onClick={() => history().push('/addItem')}>Add Item</button>}
                     </div>
+                    <button onClick={() => this.logOut()} className="btn"><img className="iconImg" src={logoutImg} /></button>
+                </div>
+                <div className="sorters">
+                    <SortingView setSortOrder={this.setSortOrder} asc={sorters.title.asc} name="title" />
+                    <SortingView setSortOrder={this.setSortOrder} asc={sorters.category.asc} name="category" />
+                    <SortingView setSortOrder={this.setSortOrder} asc={sorters.manufacturer.asc} name="manufacturer" />
+                    <button onClick={() =>
+                        this.setState({
+                            items: [...item.allItems], sorters:
+                            {
+                                title: { asc: null },
+                                category: { asc: null },
+                                manufacturer: { asc: null },
+                            }
+                        })} className="btn">Clear Filters</button>
+                </div>
+                <div className="itemsContainer">
                     <div className="middle">
-                        <div className="sorters">
-                            <SortingView setSortOrder={this.setSortOrder} asc={sorters.title.asc} name="title" />
-                            <SortingView setSortOrder={this.setSortOrder} asc={sorters.category.asc} name="category" />
-                            <SortingView setSortOrder={this.setSortOrder} asc={sorters.manufacturer.asc} name="manufacturer" />
-                            <button onClick={() =>
-                                this.setState({
-                                    items: [...item.allItems], sorters:
-                                    {
-                                        title: { asc: null },
-                                        category: { asc: null },
-                                        manufacturer: { asc: null },
-                                    }
-                                })} className="btn">Clear Filters</button>
-                        </div>
                         {item.allItems.length == 0 && <div className="centered"><p className="blueText">No Items yet</p></div>}
                         {items.length > 0 &&
                             < div className="itemsList">
@@ -137,7 +148,8 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = {
     getAllItems,
     setCurrentItemSuccess,
-    removeItem
+    removeItem,
+    logOutUser
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ItemCatalog)
