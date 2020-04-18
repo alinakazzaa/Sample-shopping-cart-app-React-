c370c8a672225b27ef57fa70b53e86b1dea7ddd4
Alina Kazakova <alinakazakova@live.ie>
2020-04-18 23:35:24 +0100

    ensure user ID is saved

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,24 +1,24 @@
 import firebase from '../database/firebase.js'
-import { SET_USERS_SUCCESS, SET_USERS_ERROR, SET_CURRENT_USER_SUCCESS, SET_CURRENT_USER_ERROR, CLEAR_CURRENT_USER, ADD_USER, UPDATE_USER } from '../constants/index.js'
+import { SET_USERS_SUCCESS, SET_USERS_ERROR, SET_CURRENT_USER_SUCCESS, SET_CURRENT_USER_ERROR, CLEAR_CURRENT_USER, ADD_USER, UPDATE_USER, CLEAR_USER_STATE, CLEAR_ITEM_STATE, CLEAR_BASKET_STATE } from '../constants/index.js'
 const db = firebase.database()
 
-export const addUser = value => {
+export const addUser = user => {
     return dispatch => {
         db.ref('/Users').push({
-            ...value,
-            confirmPassword: null,
-            admin: true
+            ...user,
+            confirmPassword: null
         }).then(data => {
 
             db.ref(`/Users/${data.key}`).update({
                 id: data.key
             })
-        })
 
-        dispatch(setCurrentUserSuccess(value))
+            dispatch(setCurrentUserSuccess({ ...user, id: data.key }))
 
-        dispatch({
-            type: ADD_USER
+            dispatch({
+                type: ADD_USER,
+                user: { ...user, id: data.key }
+            })
         })
     }
 }
@@ -72,25 +72,28 @@ export const setCurrentUserError = message => {
 }
 
 export const logOutUser = () => {
-    return {
-        type: CLEAR_CURRENT_USER
+    return dispatch => {
+
+        dispatch({ type: CLEAR_BASKET_STATE })
+        dispatch({ type: CLEAR_ITEM_STATE })
+        dispatch({ type: CLEAR_USER_STATE })
     }
+
 }
 
 export const updateUser = user => {
-    return dispatch => {
-        db.ref(`/Users/${user.id}`).update({
-            ...user
-        })
+    db.ref(`/Users/${user.id}`).update({
+        ...user
+    })
 
-        dispatch({
-            type: UPDATE_USER,
-            user
-        })
+    return {
+        type: UPDATE_USER,
+        user
     }
+}
 
-
-
+export const addOrderToUser = (userID, orderID) => {
+    db.ref(`/Users/${userID}/orders/${orderID}`).set({ id: orderID })
 }
 
 export const removeUser = user => {
